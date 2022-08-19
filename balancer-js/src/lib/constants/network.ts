cac5fd16e671afd27f9dcd399b6d406b182a63d2
Bianca Buzea <biancabuzea@Biancas-MacBook-Pro.fritz.box>
2022-08-19 15:30:12 +0200

    swap.ts example

---
+++ balancer-js/src/lib/constants/network.ts
@@ -0,0 +1,11 @@
+export enum Network {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GOERLI = 5,
+  GÖRLI = 5,
+  OPTIMISM = 10,
+  KOVAN = 42,
+  POLYGON = 137,
+  ARBITRUM = 42161,
+}
---
+++ balancer-js/src/lib/utils/index.ts
@@ -0,0 +1,11 @@
+import { getAddress } from '@ethersproject/address';
+
+export * from './errors';
+export * from './permit';
+export * from './signatures';
+export * from './assetHelpers';
+export * from './aaveHelpers';
+export * from './poolHelper';
+
+export const isSameAddress = (address1: string, address2: string): boolean =>
+  getAddress(address1) === getAddress(address2);
